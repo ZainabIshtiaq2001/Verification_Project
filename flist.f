845dac45556f8533985cb7667e5dc23c1b638b40
Muhammad Asim Javaid <24060008@lums.edu.pk>
2026-05-06 22:02:45 +0500

    Added flist.f

---
+++ flist.f
@@ -0,0 +1,11 @@
+
+./Functional/ahb3lite_pkg.sv
+./Functional/ahb_if.sv
+./Functional/mem.sv
+./Functional/design.sv
+##./Verification_Project/Functional/tb_top.sv
+##./Verification_Project/Functional/directed_tests.sv
+./Covergroups/coverage_module.sv
+./Covergroups/directed_tests_v2.sv
+##./Functional/tb_top.sv
+./Covergroups/tb_top_v2.sv
